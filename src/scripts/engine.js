f2da087a2197c7a031777d0a5595fc69c0978f91
mauriciomouraa <mauricioalb.moura@gmail.com>
2024-11-05 17:16:33 -0300

    Jogo do Detona Ralph DIO

---
+++ src/scripts/engine.js
@@ -0,0 +1,86 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+        resultEnd: document.querySelector("#resultEnd"),
+    },
+    values: {
+        gameVelocity: 800,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 30,
+    },
+    actions:{
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+}
+
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent =  state.values.currentTime;
+
+    if(state.values.currentTime <= 0){
+        /*alert("Game Over! O seu resultado foi: " + state.values.result);*/ 
+        localStorage.setItem("gameResult", state.values.result);    
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.timerId);
+        window.location.href = ("./end.html");
+    }
+    
+}
+
+function paginaFinal() {
+    const gameResult = localStorage.getItem("gameResult");
+
+    if (gameResult !== null) {
+        document.getElementById("resultEnd").textContent = `${gameResult}`;
+    } else {
+        document.getElementById("resultEnd").textContent = "0";
+    }
+
+    localStorage.removeItem("gameResult");
+}
+
+function playSound (audioName){
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.05;
+    audio.play();
+}
+
+function randomSquare (){
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition){
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            }
+        });
+    });
+}
+
+function init() {
+    addListenerHitBox();
+}
+
+init();
+
+if (window.location.pathname.includes("end.html")) {
+    paginaFinal();
+}
